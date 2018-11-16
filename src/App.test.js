67b85099cce6281c609d300ea7d3ce1f384c40dd
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-11-16 23:00:24 +0700

    ✅ use react-testing-library for test component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render } from 'react-testing-library'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  render(<App />)
+})
+
+// https://github.com/kentcdodds/react-testing-library#setup
