1f98fb1e9f1240980e17865b3ef86973ef90fa6f
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-11-23 16:53:07 +0700

    ✨ add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Switch, Route, Link } from 'react-router-dom'
+import Worker from './WebWorker'
+import ModalContainer from './Modal'
+import NewWindow from './NewWindow'
+import GreenSock from './GreenSock'
+import RenderTesting from './RenderTesting'
+import CallMobileMethod from './CallMobileMethod'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/worker">Worker</Link>
+          </li>
+          <li>
+            <Link to="/modal">Modal</Link>
+          </li>
+          <li>
+            <Link to="/new-window">NewWindow</Link>
+          </li>
+          <li>
+            <Link to="/greensock">GreenSock</Link>
+          </li>
+          <li>
+            <Link to="/render-testing">RenderTesting</Link>
+          </li>
+          <li>
+            <Link to="/call-mobile-method">CallMobileMethod</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          component={() => (
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          )}
+        />
+        <Route path="/worker" exact component={Worker} />
+        <Route path="/modal" exact component={ModalContainer} />
+        <Route path="/new-window" exact component={NewWindow} />
+        <Route path="/greensock" exact component={GreenSock} />
+        <Route path="/render-testing" exact component={RenderTesting} />
+        <Route path="/call-mobile-method" exact component={CallMobileMethod} />
+      </Switch>
     </div>
   )
 }
